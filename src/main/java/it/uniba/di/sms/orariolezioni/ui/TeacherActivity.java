e6074cb62e12c4f121623020c2e68e5130fbba96
davideC00 <dvdx97@gmail.com>
2020-01-23 11:40:53 +0100

    FIX: AddUnavailability close when rotate

--- src/main/java/it/uniba/di/sms/orariolezioni/ui/TeacherActivity.java
+++ src/main/java/it/uniba/di/sms/orariolezioni/ui/TeacherActivity.java
@@ -31,7 +31,6 @@ public class TeacherActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private Toolbar toolbar;
-    private String teacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +38,6 @@ public class TeacherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teacher);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        teacher = ((OrarioLezioniApplication) this.getApplication()).getTeacher();
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
@@ -58,9 +55,6 @@ public class TeacherActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        Bundle bundle = new Bundle();
-        bundle.putString("teacher", teacher);
-        navController.setGraph(navController.getGraph(), bundle);
 
     }
 
